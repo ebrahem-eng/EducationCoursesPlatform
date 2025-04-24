26fa2dd09e52af80476ba1c618e50dd9abacf721
Ebrahem Alwish <ebrahemalwish@MacBook-Air-Ebrahem.local>
2025-04-24 11:30:40 +0300

    admin student manage fix

--- app/Http/Middleware/StudentAuthMiddleware.php
+++ app/Http/Middleware/StudentAuthMiddleware.php
@@ -14,10 +14,12 @@ class StudentAuthMiddleware
         }
 
         if (Auth::guard('student')->user()->block) {
+            Auth::guard('student')->logout();
             return redirect()->route('student.login.page')->with('error_message', 'Your account has been blocked');
         }
 
         if (Auth::guard('student')->user()->status == 0) {
+            Auth::guard('student')->logout();
             return redirect()->route('student.login.page')->with('error_message', 'Your account is not activated yet');
         }
 
